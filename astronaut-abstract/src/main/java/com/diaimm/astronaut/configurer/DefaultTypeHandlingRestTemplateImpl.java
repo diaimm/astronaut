7b2db870a1390cc832ca5fe5fd2f8384322cf23b
봉구(이성준) <diaimm@coupang.com>
2016-08-25 23:01:19 +0900

    Simplify some codes

--- astronaut-abstract/src/main/java/com/diaimm/astronaut/configurer/DefaultTypeHandlingRestTemplateImpl.java
+++ astronaut-abstract/src/main/java/com/diaimm/astronaut/configurer/DefaultTypeHandlingRestTemplateImpl.java
@@ -107,8 +107,11 @@ public class DefaultTypeHandlingRestTemplateImpl extends RestTemplate implements
 						if (converter.canRead(responseClass, null)) {
 							allSupportedMediaTypes.addAll(getSupportedMediaTypes(converter));
 						}
-					} else if (converter instanceof GenericHttpMessageConverter) {
-
+						
+						continue;
+					} 
+					
+					if (converter instanceof GenericHttpMessageConverter) {
 						GenericHttpMessageConverter<?> genericConverter = (GenericHttpMessageConverter<?>) converter;
 						if (genericConverter.canRead(responseType, null, null)) {
 							allSupportedMediaTypes.addAll(getSupportedMediaTypes(converter));
@@ -155,54 +158,67 @@ public class DefaultTypeHandlingRestTemplateImpl extends RestTemplate implements
 			super(responseType);
 			if (requestBody instanceof HttpEntity) {
 				this.requestEntity = (HttpEntity<?>) requestBody;
-			} else if (requestBody != null) {
+				return;
+			} 
+			
+			if (requestBody != null) {
 				this.requestEntity = new HttpEntity<Object>(requestBody);
-			} else {
-				this.requestEntity = HttpEntity.EMPTY;
+				return;
 			}
+			
+			this.requestEntity = HttpEntity.EMPTY;
 		}
 
 		@Override
 		@SuppressWarnings("unchecked")
 		public void doWithRequest(ClientHttpRequest httpRequest) throws IOException {
 			super.doWithRequest(httpRequest);
+
 			if (!requestEntity.hasBody()) {
 				HttpHeaders httpHeaders = httpRequest.getHeaders();
 				HttpHeaders requestHeaders = requestEntity.getHeaders();
 				if (!requestHeaders.isEmpty()) {
 					httpHeaders.putAll(requestHeaders);
 				}
+				
 				if (httpHeaders.getContentLength() == -1) {
 					httpHeaders.setContentLength(0L);
 				}
-			} else {
-				Object requestBody = requestEntity.getBody();
-				Class<?> requestType = requestBody.getClass();
-				HttpHeaders requestHeaders = requestEntity.getHeaders();
-				MediaType requestContentType = requestHeaders.getContentType();
-				for (HttpMessageConverter<?> messageConverter : getMessageConverters()) {
-					if (messageConverter.canWrite(requestType, requestContentType)) {
-						if (!requestHeaders.isEmpty()) {
-							httpRequest.getHeaders().putAll(requestHeaders);
-						}
-						if (logger.isDebugEnabled()) {
-							if (requestContentType != null) {
-								logger.debug("Writing [" + requestBody + "] as \"" + requestContentType + "\" using [" + messageConverter + "]");
-							} else {
-								logger.debug("Writing [" + requestBody + "] using [" + messageConverter + "]");
-							}
-
-						}
-						((HttpMessageConverter<Object>) messageConverter).write(requestBody, requestContentType, httpRequest);
-						return;
-					}
-				}
-				String message = "Could not write request: no suitable HttpMessageConverter found for request type [" + requestType.getName() + "]";
-				if (requestContentType != null) {
-					message += " and content type [" + requestContentType + "]";
-				}
-				throw new RestClientException(message);
+				
+				return;
 			}
+
+			Object requestBody = requestEntity.getBody();
+			Class<?> requestType = requestBody.getClass();
+			HttpHeaders requestHeaders = requestEntity.getHeaders();
+			MediaType requestContentType = requestHeaders.getContentType();
+			for (HttpMessageConverter<?> messageConverter : getMessageConverters()) {
+				if (!messageConverter.canWrite(requestType, requestContentType)) {
+					continue;
+				}
+				
+				if (!requestHeaders.isEmpty()) {
+					httpRequest.getHeaders().putAll(requestHeaders);
+				}
+				
+				if (logger.isDebugEnabled()) {
+					if (requestContentType != null) {
+						logger.debug("Writing [" + requestBody + "] as \"" + requestContentType + "\" using [" + messageConverter + "]");
+					} else {
+						logger.debug("Writing [" + requestBody + "] using [" + messageConverter + "]");
+					}
+
+				}
+				
+				((HttpMessageConverter<Object>) messageConverter).write(requestBody, requestContentType, httpRequest);
+				return;
+			}
+			
+			String message = "Could not write request: no suitable HttpMessageConverter found for request type [" + requestType.getName() + "]";
+			if (requestContentType != null) {
+				message += " and content type [" + requestContentType + "]";
+			}
+			throw new RestClientException(message);
 		}
 	}
 }
