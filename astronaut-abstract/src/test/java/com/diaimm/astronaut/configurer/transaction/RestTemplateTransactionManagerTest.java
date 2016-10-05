3bd1c623f321315936c94b2ab0c21b95a74518a9
봉구(이성준) <diaimm@coupang.com>
2016-10-05 22:32:19 +0900

    add unit test

--- astronaut-abstract/src/test/java/com/diaimm/astronaut/configurer/transaction/RestTemplateTransactionManagerTest.java
+++ astronaut-abstract/src/test/java/com/diaimm/astronaut/configurer/transaction/RestTemplateTransactionManagerTest.java
@@ -1,7 +1,9 @@
 package com.diaimm.astronaut.configurer.transaction;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.springframework.transaction.HeuristicCompletionException;
 import org.springframework.transaction.support.DefaultTransactionStatus;
 
 import com.diaimm.astronaut.configurer.transaction.RestTemplateTransactionObject.TransactionCommand;
@@ -10,14 +12,15 @@ public class RestTemplateTransactionManagerTest {
 	@Test
 	public void testAll() {
 		RestTemplateTransactionManager target = new RestTemplateTransactionManager();
-		
+
 		TransactionCommand commitCommand = Mockito.mock(TransactionCommand.class);
 		TransactionCommand rollbackCommand = Mockito.mock(TransactionCommand.class);
-		
+
+		target.doBegin(null, null);
 		target.doGetTransaction();
 		RestTemplateTransactionObject transactionObject = target.getTransactionObject();
 		transactionObject.pushToCallStack(commitCommand, rollbackCommand);
-		
+
 		target.doCommit(Mockito.mock(DefaultTransactionStatus.class));
 		Mockito.verify(commitCommand).execute();
 		Mockito.verify(rollbackCommand, Mockito.never()).execute();
@@ -29,5 +32,28 @@ public class RestTemplateTransactionManagerTest {
 		target.doRollback(Mockito.mock(DefaultTransactionStatus.class));
 		Mockito.verify(rollbackCommand).execute();
 		Mockito.verify(commitCommand, Mockito.never()).execute();
+	}
+
+	@Test
+	public void executeTransactionCommandsAllTest() {
+		RestTemplateTransactionManager target = new RestTemplateTransactionManager();
+		RestTemplateTransactionObject transactionObject = (RestTemplateTransactionObject) target.doGetTransaction();
+		transactionObject.getCommits();
+		
+		target.doCommit(null);
+		
+		TransactionCommand withException = Mockito.mock(TransactionCommand.class);
+		try {
+			transactionObject.getCommits().add(withException);
+		} catch(Exception e){
+			Assert.fail();
+		}
+		
+		Mockito.doThrow(new IllegalStateException("test")).when(withException).execute();
+		try {
+			transactionObject.getCommits().add(withException);
+		} catch(Exception e){
+			Assert.assertEquals(HeuristicCompletionException.class, e.getClass());
+		}
 	}
 }
